987437ac5d3c56996fcf5c99b79ab53fe89e9924
Shichang(Alan) Ye <shichangye@outlook.com>
2023-03-24 19:42:36 -0700

    Add LoginActivity, RegisterActivity, and NannyInfoActivity and corresponding xml documents.

---
+++ app/src/main/java/edu/northeastern/myapplication/authorisation/RegisterActivity.java
@@ -0,0 +1,27 @@
+package edu.northeastern.myapplication.authorisation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import edu.northeastern.myapplication.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    TextView registerUsername;
+    TextView registerPassword;
+    TextView registerConfirmPassword;
+    Button registerBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        registerUsername = findViewById(R.id.registerUsername);
+        registerPassword = findViewById(R.id.registerPassword);
+        registerConfirmPassword = findViewById(R.id.registerCofirmPassword);
+        registerBtn = findViewById(R.id.registerBtn);
+    }
+}

e0bc81fe9261c208b998d1995e07fcc10cf169a0
LinHanMSCS <linhanmscs@gmail.com>
2023-04-04 19:04:51 -0700

    Add java doc for MyInfo activity

--- app/src/main/java/edu/northeastern/myapplication/MyInfoActivity.java
+++ app/src/main/java/edu/northeastern/myapplication/MyInfoActivity.java
@@ -7,12 +7,21 @@ import android.widget.TextView;
 
 import edu.northeastern.myapplication.entity.User;
 
+/**
+ * MyInfo Activity.
+ */
 public class MyInfoActivity extends AppCompatActivity {
     private TextView inputNameTextView;
     private TextView inputEmailTextView;
     private TextView inputLocationTextView;
     private User currentUser;
 
+    /**
+     * Called when the activity is starting.
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle contains the data it most
+     *                           recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

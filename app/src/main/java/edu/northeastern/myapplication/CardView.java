7acb2f3d9e1f764aa434a2d772ca8ed56d015119
Manpingzz <zhao.manp@northeastern.edu>
2023-04-09 00:54:08 -0700

    Update RecylerView

--- app/src/main/java/edu/northeastern/myapplication/CardView.java
+++ app/src/main/java/edu/northeastern/myapplication/CardView.java
@@ -1,10 +1,18 @@
 package edu.northeastern.myapplication;
+import edu.northeastern.myapplication.entity.Tip;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,9 +36,32 @@ public class CardView extends AppCompatActivity {
         tipDataList = new ArrayList<>();
 
 
-        adapter = new CardViewAdapter(tipDataList);
+        adapter = new CardViewAdapter(this, tipDataList);
         recyclerView.setAdapter(adapter);
+
+//        loadTipsFromFirebase();
     }
+
+//    private void loadTipsFromFirebase() {
+//        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("tips");
+//
+//        databaseReference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                tipDataList.clear();
+//                for (DataSnapshot tipSnapshot : dataSnapshot.getChildren()) {
+//                    Tip tip = tipSnapshot.getValue(Tip.class);
+//                    tipDataList.add(tip);
+//                }
+//                adapter.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//                // Handle error
+//            }
+//        });
+//    }
 }
 
 
--- app/src/main/java/edu/northeastern/myapplication/CardViewAdapter.java
+++ app/src/main/java/edu/northeastern/myapplication/CardViewAdapter.java
@@ -1,5 +1,7 @@
 package edu.northeastern.myapplication;
 
+import edu.northeastern.myapplication.entity.Tip;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,20 +12,24 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.List;
 
 public class CardViewAdapter extends RecyclerView.Adapter<CardViewAdapter.ViewHolder> {
 
+    private Context context;
     private List<Tip> tipDataList;
 
-    public CardViewAdapter(List<Tip> tipDataList) {
+    public CardViewAdapter(Context context, List<Tip> tipDataList) {
+        this.context = context;
         this.tipDataList = tipDataList;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_card_view, parent,false);
+        View itemView = LayoutInflater.from(context).inflate(R.layout.activity_card_view, parent,false);
         final ViewHolder holder = new ViewHolder(itemView);
         return holder;
     }
@@ -31,12 +37,16 @@ public class CardViewAdapter extends RecyclerView.Adapter<CardViewAdapter.ViewHo
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         Tip currentTip = tipDataList.get(position);
-//        holder.image.setImageResource(currentTip.getUrl());
-        holder.image.setImageResource(currentTip.getImageId());
         holder.title.setText(currentTip.getTitle());
-        holder.username.setText(currentTip.getUsername());
+        holder.username.setText(currentTip.getUserId());
+        String imageUrl = currentTip.getPictureUrl().toString();
 
+        Glide.with(context)
+                .load(imageUrl)
+                .centerCrop()
+                .into(holder.image);
     }
+
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         View tipView;
@@ -58,5 +68,16 @@ public class CardViewAdapter extends RecyclerView.Adapter<CardViewAdapter.ViewHo
         return tipDataList.size();
     }
 
+    /**
+     * Update data of the tipDataList
+     *
+     * @param tipDataList
+     */
+
+    public void setTipDataList(List<Tip> tipDataList) {
+        this.tipDataList = tipDataList;
+        notifyDataSetChanged();
+    }
 
 }
+

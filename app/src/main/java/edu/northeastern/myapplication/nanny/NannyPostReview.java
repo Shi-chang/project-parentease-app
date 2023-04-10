54983f9efd02cebeea0d92b84b0fe577f7dfda7f
Chen Chen <chen.chencc@outlook.com>
2023-04-10 08:22:43 -0700

    Refactor Nanny related classes to /nanny and updated AndroidManifest.xml

---
+++ app/src/main/java/edu/northeastern/myapplication/nanny/NannyPostReview.java
@@ -0,0 +1,17 @@
+package edu.northeastern.myapplication.nanny;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.northeastern.myapplication.R;
+
+public class NannyPostReview extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_nanny_review);
+        }
+    }
+

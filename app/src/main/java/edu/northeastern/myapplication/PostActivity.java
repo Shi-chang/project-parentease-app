c8503bdd8b2de0995e0aee81cb9990959c441c39
LinHanMSCS <linhanmscs@gmail.com>
2023-04-10 17:33:49 -0700

    Add navigation imageview for Post Activity

--- app/src/main/java/edu/northeastern/myapplication/PostActivity.java
+++ app/src/main/java/edu/northeastern/myapplication/PostActivity.java
@@ -2,9 +2,12 @@ package edu.northeastern.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import edu.northeastern.myapplication.entity.User;
 import edu.northeastern.myapplication.nanny.NannyInformation;
@@ -17,6 +20,10 @@ public class PostActivity extends AppCompatActivity {
     Button btnPostATip;
     Button btnPostANannyAd;
     User user;
+    private ImageView browseImageView;
+    private ImageView nannyShareImageView;
+    private ImageView tipsShareImageView;
+    private ImageView myAccountImageView;
 
     /**
      * Called when the activity is starting.
@@ -25,6 +32,7 @@ public class PostActivity extends AppCompatActivity {
      *                           previously being shut down then this Bundle contains the data it most
      *                           recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
      */
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,5 +58,48 @@ public class PostActivity extends AppCompatActivity {
             intent.putExtras(bundle);
             startActivity(intent);
         });
+
+        browseImageView = findViewById(R.id.browseImageView);
+        browseImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PostActivity.this, HomeActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putParcelable("user", user);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+
+        nannyShareImageView = findViewById(R.id.nannyImageView);
+
+        tipsShareImageView = findViewById(R.id.tipsImageView);
+        tipsShareImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                refreshPostActivity();
+            }
+        });
+
+        myAccountImageView = findViewById(R.id.myAccountImageView);
+        myAccountImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PostActivity.this, MyInfoActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putParcelable("user", user);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void refreshPostActivity() {
+        Intent intent = new Intent(this, PostActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putParcelable("user", user);
+        intent.putExtras(bundle);
+        startActivity(intent);
+        finish();
     }
 }

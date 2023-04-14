e3ec93259f60708c7276af3aad4d39123cdfb44d
Chen Chen <chen.chencc@outlook.com>
2023-04-13 21:37:09 -0700

    Revert "Added Review card adapter"
    This reverts commit 87c1c932fde16c6084d92ca641db09eddbe2970f.

--- app/src/main/java/edu/northeastern/myapplication/nanny/NannyCardAdapter.java
+++ app/src/main/java/edu/northeastern/myapplication/nanny/NannyCardAdapter.java
@@ -51,15 +51,13 @@ public class NannyCardAdapter extends RecyclerView.Adapter<NannyCardAdapter.Nann
 
     //View Holder: NannyHolder
     class NannyHolder extends RecyclerView.ViewHolder{
-        private TextView tv_name, tv_location, tv_yoe, tv_hourlyRate;
+        private TextView tv_name, tv_location, tv_yoe;
 
         public NannyHolder(@NonNull View itemView, RecyclerViewInterface recyclerViewInterface) {
             super(itemView);
             tv_name = itemView.findViewById(R.id.tv_nameCard);
             tv_yoe = itemView.findViewById(R.id.tv_yoe);
-            tv_hourlyRate = itemView.findViewById(R.id.tv_hourlyRate);
             tv_location = itemView.findViewById(R.id.tv_location);
-
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -79,7 +77,6 @@ public class NannyCardAdapter extends RecyclerView.Adapter<NannyCardAdapter.Nann
             //TODO
             tv_name.setText(nanny.getUsername());
             tv_yoe.setText("YOE: "+ String.valueOf(nanny.getYoe()));
-            tv_hourlyRate.setText("Hourly Rate: "+ String.valueOf(nanny.getHourlyRate()));
             tv_location.setText("City: " + nanny.getCity());
         }
     }
